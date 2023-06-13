83b671a837ea369ce2c51869e4746635ea469cca
Brayden Kim <102700164+YHKimmm@users.noreply.github.com>
2023-06-13 14:45:27 -0700

    Time: 154 ms (11.11%) | Memory: 42.6 MB (34.95%) - LeetSync

--- game-of-life/game-of-life.cs
+++ game-of-life/game-of-life.cs
@@ -1,55 +1,48 @@
 public class Solution {
     public void GameOfLife(int[][] board) {
-        int[][] directions = new int[][] {
-        new int[] {-1, 0},
-        new int[] {-1, 1},
-        new int[] {0, 1},
-        new int[] {1, 1},
-        new int[] {1, 0},
-        new int[] {1, -1},
-        new int[] {0, -1},
-        new int[] {-1, -1}
+        // original | new
+        //      0       0       0
+        //      1       0       -1 ->Math.Abs(1)
+        //      0       1       2
+        //      1       1       1
+        int[][] directions = new int[][]
+        {
+            new int[] {-1,-1},
+            new int[] {-1,0},
+            new int[] {-1,1},
+            new int[] {0,1},
+            new int[] {0,-1},
+            new int[] {1,-1},
+            new int[] {1, 0},
+            new int[] {1,1}
         };
-
         int m = board.Length;
         int n = board[0].Length;
 
-        for (int i = 0; i < m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for (int j = 0; j < n; j++)
+            for(int j = 0; j < n; j++)
             {
-                // Maintaining a counter of live cells in the original matrix
                 int liveCount = 0;
-
-                foreach (int[] direction in directions)
+                foreach(var dir in directions)
                 {
-                    // Determining what our new row would be after taking a direction
-                    int nr = i + direction[0];
-
-                    // Determining what our new column would be after taking a direction
-                    int nc = j + direction[1];
-
-                    // Ensuring that our new_row and new_column
-                    // are within the bounds of the matrix
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && Math.Abs(board[nr][nc]) == 1)
+                    int x = dir[0] + i;
+                    int y = dir[1] + j;
+                    if(x >= 0 && x < m && y >= 0 && y < n && Math.Abs(board[x][y]) == 1)
                     {
-                        int value = board[nr][nc];
                         liveCount++;
                     }
                 }
-
-                if (board[i][j] == 1)
+                if(board[i][j] == 1)
                 {
-                    // case where live cell dies from under/over population
-                    if (liveCount < 2 || liveCount > 3)
+                    if(liveCount < 2 || liveCount > 3)
                     {
                         board[i][j] = -1;
                     }
                 }
-                else
+                else if(board[i][j] == 0)
                 {
-                    // case of dead cell resurrection
-                    if (liveCount == 3)
+                    if(liveCount == 3)
                     {
                         board[i][j] = 2;
                     }
@@ -57,15 +50,15 @@ public class Solution {
             }
         }
 
-        for (int i = 0; i < m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for (int j = 0; j < n; j++)
+            for(int j = 0; j < n; j++)
             {
-                if (board[i][j] == -1)
+                if(board[i][j] == -1)
                 {
                     board[i][j] = 0;
                 }
-                else if (board[i][j] == 2)
+                else if(board[i][j] == 2)
                 {
                     board[i][j] = 1;
                 }

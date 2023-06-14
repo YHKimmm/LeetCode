c8c34ddc3d33099ddb2bc4a4dd823d75f9a2280e
Brayden Kim <102700164+YHKimmm@users.noreply.github.com>
2023-06-13 19:04:26 -0700

    Time: 85 ms (73.93%) | Memory: 43.3 MB (52.32%) - LeetSync

---
+++ ransom-note/ransom-note.cs
@@ -0,0 +1,53 @@
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        if(ransomNote.Length == 1 && magazine.Length == 1)
+        {
+            if(!ransomNote.StartsWith(magazine))
+            {
+                return false;
+            }
+        }
+        Dictionary<char, int> ransomDict = new();
+        Dictionary<char, int> magazineDict = new();
+
+        for(int i = 0; i < ransomNote.Length; i++)
+        {
+            if(ransomDict.ContainsKey(ransomNote[i]))
+            {
+                ransomDict[ransomNote[i]] += 1;
+            }
+            else
+            {
+                ransomDict[ransomNote[i]] = 1;
+            }
+        }
+
+        for(int i = 0; i < magazine.Length; i++)
+        {
+            if(magazineDict.ContainsKey(magazine[i]))
+            {
+                magazineDict[magazine[i]] += 1;
+            }
+            else
+            {
+                magazineDict[magazine[i]] = 1;
+            }
+        }
+        // {a:2, c:1} {a:2, b:1} -> F
+        // {a:2} {a:2, b:1} -> T
+        // "aac" "aab"
+        bool valid = false;
+        foreach (var ele in ransomDict)
+        {
+            if (magazineDict.ContainsKey(ele.Key) && ele.Value <= magazineDict[ele.Key])
+            {
+                valid = true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return valid;
+    }
+}

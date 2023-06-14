0f04a40fc6092511447142216a9d4b462978edfc
Brayden Kim <102700164+YHKimmm@users.noreply.github.com>
2023-06-14 14:41:49 -0700

    Time: 36 ms (75.51%) | Memory: 29.4 MB (23.22%) - LeetSync

--- happy-number/happy-number.cs
+++ happy-number/happy-number.cs
@@ -1,40 +1,23 @@
 public class Solution {
     public bool IsHappy(int n) {
-        if(n == 1 || n == 7 || n == 1111111)
+        // n = 2 -> 4 16 37 58 89 145 42 20 4
+        HashSet<int> set = new();
+        while(n != 1)
         {
-            return true;
-        }
-        Dictionary<string, int> dict = new();
-        // {19:82, 82:68, 68:100, 100:1}
-        bool valid = false;
-        string newStr = n.ToString();
-        int count = 0;
-        while (count < 100)
-        {
-            newStr = n.ToString(); // "19"
             int sum = 0;
-            var charArr = newStr.ToCharArray(); // 1 9
-            if (charArr.Length == 1)
+            string newStr = n.ToString();
+            foreach(char c in newStr)
+            {
+                int newInt = (int)c - '0';
+                sum += (newInt * newInt);
+            }
+            if(set.Contains(sum))
             {
                 return false;
             }
-            for (int i = 0; i < charArr.Length; i++)
-            {
-                int intValue = (int)(charArr[i] - '0');
-                sum += (intValue * intValue);
-            }
+            set.Add(sum);
             n = sum;
-            if (!dict.ContainsKey(newStr))
-            {
-                dict[newStr] = sum;
-            }
-            if (dict[newStr] == 1)
-            {
-                valid = true;
-                return valid;
-            }
-            count++;
-        }
-        return valid;
+        } 
+        return true;
     }
 }

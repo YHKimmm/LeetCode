1868554aef22446dd978180b7c704a294193b7ad
Brayden Kim <102700164+YHKimmm@users.noreply.github.com>
2023-06-14 23:49:36 -0700

    Time: 184 ms (52.81%) | Memory: 65.6 MB (6.77%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.cs
+++ longest-consecutive-sequence/longest-consecutive-sequence.cs
@@ -5,22 +5,21 @@ public class Solution {
         {
             dict[num] = true;
         }
-        // {100: true, 4:true, 200:true, 1:true, 3:true, 2:true}
-        int longest = 0;
+        int ans = 0;
         foreach(var num in dict.Keys)
         {
-            if(!dict.ContainsKey(num - 1))
+            if(!dict.ContainsKey(num-1))
             {
-                int count = 1;
                 int nextVal = num + 1;
+                int count = 1;
                 while(dict.ContainsKey(nextVal))
                 {
                     count++;
                     nextVal++;
                 }
-                longest = Math.Max(longest, count);
+                ans = Math.Max(ans, count);
             }
         }
-        return longest;
+        return ans;
     }
 }

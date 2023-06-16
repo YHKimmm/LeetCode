908dd6d561280a34572572d9b3b82890fe76d135
Brayden Kim <102700164+YHKimmm@users.noreply.github.com>
2023-06-16 16:20:33 -0700

    Time: 165 ms (90.10%) | Memory: 52.7 MB (29.30%) - LeetSync

---
+++ merge-intervals/merge-intervals.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        List<List<int>> res = new List<List<int>>();
+
+        for (int i = 0; i < intervals.Length; i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            while (i < intervals.Length - 1 && end >= intervals[i + 1][0])
+            {
+                end = Math.Max(end, intervals[i + 1][1]);
+                i++;
+            }
+
+            res.Add(new List<int> { start, end });
+        }
+
+        return res.Select(x => x.ToArray()).ToArray();
+    }
+}
